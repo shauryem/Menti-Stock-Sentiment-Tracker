397a267170ea269eff05ad33680538e06219a185
Shaurye Mahajan <43662406+shauryem@users.noreply.github.com>
2021-05-04 19:17:42 -0700

    Initial commit from Spring

---
+++ src/main/java/stocks/Menti/MentiApplication.java
@@ -0,0 +1,13 @@
+package stocks.Menti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stocks/Menti/MentiApplicationTests.java
@@ -0,0 +1,13 @@
+package stocks.Menti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
