08fe139593fe7f0853c4b3e06e233281a80d4bab
shaurye <shaurye@ucsb.edu>
2021-05-04 19:23:42 -0700

    inital test comment

--- src/main/java/stocks/Menti/MentiApplication.java
+++ src/main/java/stocks/Menti/MentiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MentiApplication {
 
+	//test comment for first commit 
 	public static void main(String[] args) {
 		SpringApplication.run(MentiApplication.class, args);
 	}
